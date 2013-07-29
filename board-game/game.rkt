d818e2f20e66cce47e916fd9d80a4ccc926e07dd
Johan Lindberg <johan@pulp.se>
2013-07-29 23:43:08 +0200

    Modified the draw-board function so that it reads of a linked list.
    In order to avoid double keeping a lot of information about
    which paths are allowed and which player is where I made a simple
    linked list data structure which also contains the screen
    coordinates for drawing. It also keeps which nodes you are
    allowed to move to and whether or not it is a stone node or the
    final (goal) node.

--- board-game/game.rkt
+++ board-game/game.rkt
@@ -6,6 +6,41 @@
 
 (define current-player 1)
 
+;; A list of nodes where each node is := (index stone? goal? (x y) (next))
+(define path 
+  (list (list 0 #f #f (list 53 103) (list 1))
+        (list 1 #f #f (list 90 120) (list 0 2))
+        (list 2 #t #f (list 131 125) (list 1 3))
+        (list 3 #f #f (list 170 140) (list 2 4))
+        (list 4 #f #f (list 190 174) (list 3 5 13))
+        (list 5 #f #f (list 223 154) (list 4 6))
+        (list 6 #f #f (list 256 135) (list 5 7))
+        (list 7 #t #f (list 284 108) (list 6 8))
+        (list 8 #f #f (list 310 80) (list 7 9))
+        (list 9 #f #f (list 340 55) (list 8 10))
+        (list 10 #f #f (list 378 53) (list 9 11))
+        (list 11 #t #f (list 396 88) (list 10 12))
+        (list 12 #f #f (list 374 120) (list 11 26))
+              
+        (list 13 #f #f (list 163 203) (list 4 14))
+        (list 14 #t #f (list 127 220) (list 13 15))
+        (list 15 #f #f (list 131 125) (list 14 16))
+        (list 16 #f #f (list 88 225) (list 15 17))
+        (list 17 #f #f (list 85 264) (list 16 18))
+        (list 18 #f #f (list 120 280) (list 17 19))
+        (list 19 #t #f (list 157 290) (list 18 20))
+        (list 20 #f #f (list 195 294) (list 19 21))
+        (list 21 #f #f (list 233 305) (list 10 22))
+        (list 22 #t #f (list 271 305) (list 21 23))
+        (list 23 #f #f (list 309 300) (list 22 24))
+        (list 24 #f #f (list 334 270) (list 23 25))
+        (list 25 #t #f (list 345 232) (list 24 26))
+        (list 26 #f #f (list 309 300) (list 25 27))
+        (list 27 #f #f (list 355 195) (list 12 26 28))
+        (list 28 #f #f (list 364 157) (list 27 29))
+        
+        (list 29 #f #t (list 402 161) (list 28))))
+
 ;; Current status
 (define (get-current-status-label)
   (string-append "Player " (number->string current-player) "'s turn"))
@@ -23,61 +58,29 @@
     (send dc draw-ellipse 18 y 10 10)))
 
 (define (draw-board canvas dc)
-          ;;; Draw the game board
-          ;;; This is more or less a direct translation of a drawing.
-          
-          (send dc set-pen "black" 3 'solid)
-          (send dc set-brush "white" 'transparent)
+  (send dc set-pen "black" 3 'solid)
+  (send dc set-brush "white" 'transparent)
 
-          ;; Start arrow
-          (send dc draw-line 33 123 53 123)
-          (send dc draw-line 43 118 53 123)
-          (send dc draw-line 43 128 53 123)
+  ;; Start arrow
+  (send dc draw-line 33 123 53 123)
+  (send dc draw-line 43 118 53 123)
+  (send dc draw-line 43 128 53 123)
           
-          ;; Goal arrow
-          (send dc draw-line 442 181 462 181)
-          (send dc draw-line 452 176 462 181)
-          (send dc draw-line 452 186 462 181)
-                    
-          ;; Regular board positions
-          (send dc draw-ellipse 53 103 40 40)
-          (send dc draw-ellipse 90 120 40 40)
-          (send dc draw-ellipse 170 140 40 40)
-          
-          (send dc draw-ellipse 190 174 40 40)
+  ;; Goal arrow
+  (send dc draw-line 442 181 462 181)
+  (send dc draw-line 452 176 462 181)
+  (send dc draw-line 452 186 462 181)
 
-          (send dc draw-ellipse 223 154 40 40)
-          (send dc draw-ellipse 256 135 40 40)
-          (send dc draw-ellipse 310 80 40 40)
-          (send dc draw-ellipse 340 55 40 40)
-          (send dc draw-ellipse 378 53 40 40)
-          
-          (send dc draw-ellipse 374 120 40 40)
-          
-          (send dc draw-ellipse 163 203 40 40)
-          (send dc draw-ellipse 88 225 40 40)
-          (send dc draw-ellipse 85 264 40 40)          
-          (send dc draw-ellipse 120 280 40 40)
-          (send dc draw-ellipse 195 294 40 40)
-          (send dc draw-ellipse 233 305 40 40)
-          (send dc draw-ellipse 309 300 40 40)
-          (send dc draw-ellipse 334 270 40 40)
-          (send dc draw-ellipse 355 195 40 40)
-          (send dc draw-ellipse 364 157 40 40)
-          
-          (send dc draw-ellipse 402 161 40 40)
-
-          ;;; Stone positions
-          (send dc set-brush "red" 'solid)
-
-          (send dc draw-ellipse 131 125 40 40)
-          (send dc draw-ellipse 127 220 40 40)
-          (send dc draw-ellipse 157 290 40 40)
-          (send dc draw-ellipse 271 305 40 40)
-          (send dc draw-ellipse 345 232 40 40)
-
-          (send dc draw-ellipse 284 108 40 40)
-          (send dc draw-ellipse 396 88 40 40))
+  (for ([node path])
+    ; node := (index stone? goal? (x y) (next))
+    (if (second node)
+        (send dc set-brush "red" 'solid)
+        (send dc set-brush "white" 'transparent))
+    
+    (send dc draw-ellipse
+          (first (fourth node))  ; x
+          (second (fourth node)) ; y
+          40 40)))
 
 ;;; GUI definition
 ;;; --------------
@@ -112,5 +115,11 @@
 (define current-status-label (new message%
                                   [label (get-current-status-label)]
                                   [parent bottom-panel]))
+(define die (new button%
+                 [label "Push me!"]
+                 [parent bottom-panel]
+                 [callback
+                  (lambda (button event)
+                    (send die set-label (number->string (+ (random 6) 1))))]))
 
 (send main-frame show #t)
